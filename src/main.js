709a90258c19bbc81f6549ba2aa4044b425e8ba9
Dean <zgh0711@163.com>
2019-04-26 15:54:06 +0800

    加入自定义指令 v-reset-page，以解决 ios 12 中键盘收起后底部有留白的问题

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
-import './assets/common.css'
+import './assets/css/core.min.css'
+import './assets/css/common.css'
 
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import apis from './apis'
+import './utils/directive'
 import wxShare from './utils/wxShare'
 import myUtils from './utils/myUtils'
-import {Button, Toast} from 'vant'
+import {PullRefresh, Lazyload, Button, Toast, List} from 'vant'
 
 Vue.config.productionTip = false
 Vue.prototype.apis = apis
 Vue.prototype.wxShare = wxShare
 Vue.prototype.myUtils = myUtils
 
-Vue.use(Button)
+Vue.use(PullRefresh)
+   .use(Button)
    .use(Toast)
+   .use(List)
+   .use(Lazyload,{
+      error: require('./assets/img/img_placeholder.svg'),
+      loading: require('./assets/img/img_placeholder.svg'),
+   })
 
 new Vue({
   router,
---
+++ src/utils/directive.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+/**
+ * 自定义指令 v-reset-page，以解决 iOS 12 中键盘收起后页面底部有留白的问题
+ */
+
+Vue.directive('resetPage', {
+  inserted: function (el) {
+    // 该方法有时候会出现点击了键盘右上角完成按钮，键盘收起又弹出的情况
+    // el.addEventListener('blur', function () {
+    //   if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+    //     let currentPosition, timer
+    //     let speed = 1//页面滚动距离
+    //     timer = setInterval(function () {
+    //       currentPosition = document.documentElement.scrollTop || document.body.scrollTop
+    //       currentPosition -= speed
+    //       window.scrollTo(0, currentPosition)//页面向上滚动
+    //       currentPosition += speed //speed变量
+    //       window.scrollTo(0, currentPosition)//页面向下滚动
+    //       clearInterval(timer)
+    //     }, 100)
+    //   }
+    // })
+    
+    document.body.addEventListener('focusout', () => {
+      if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+        //软键盘收起的事件处理
+        setTimeout(() => {
+          const scrollHeight = document.documentElement.scrollTop || document.body.scrollTop || 0
+          window.scrollTo(0, Math.max(scrollHeight - 1, 0))
+        }, 100)
+      }
+    })
+  }
+})
