f1870c5becac7168fbb0a5c59371210c08ada76a
Dean <zgh0711@163.com>
2019-08-14 15:36:46 +0800

    build 打包时对文件进行 Gzip 压缩，进一步减小代码体积，提高加载速度

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,28 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
   //访问服务器上目录的地址，在程序部署之前根据需要换掉
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dc/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   
   productionSourceMap: false,
   
   //移除 prefetch 插件
   chainWebpack: config =>{
     config.plugins.delete('prefetch')
-  }
-
+  },
+  
+  //build 打包时对文件进行 Gzip 压缩
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|.\css/, //匹配文件名
+            threshold: 10240,//对超过10k的数据压缩
+            deleteOriginalAssets: false, //不删除源文件
+          }),
+        ],
+      }
+    }
+  },
 }
